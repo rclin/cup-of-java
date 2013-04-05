0316368d2c561cbe4957236f45152cf025edf082
Richard Lin <rclin_cal@yahoo.com>
2013-04-05 10:41:39 -0700

    Added named constant to Fibonacci

--- the-java-programming-language/a-quick-tour/Fibonacci.java
+++ the-java-programming-language/a-quick-tour/Fibonacci.java
@@ -1,12 +1,13 @@
 public class Fibonacci {
-	/** Print out the Fibonacci sequence for values < 50 */	
+	static final int MAX = 100;
+	/** Print out the Fibonacci sequence for values < MAX */	
 	public static void main(String[] args) {
 		int lo = 1;
 		int hi = 1;
 
-		System.out.println("Fibonacci sequence for values < 50:");
+		System.out.println("Fibonacci sequence for values < " + MAX);
 		System.out.println(lo);
-		while (hi < 50) {
+		while (hi < MAX) {
 			System.out.println(hi);
 			hi = lo + hi;
 			lo = hi - lo;
