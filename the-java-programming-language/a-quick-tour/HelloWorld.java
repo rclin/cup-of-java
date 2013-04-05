378a8d24f27cd28c301750819c7bbb03b4a86bdc
Richard Lin <rclin_cal@yahoo.com>
2013-04-05 10:44:36 -0700

    Added named constant to HelloWorld

--- the-java-programming-language/a-quick-tour/HelloWorld.java
+++ the-java-programming-language/a-quick-tour/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 
+	static final String greeting = "Hello world!";
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println(greeting);
 	}
 }
