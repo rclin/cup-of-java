25bc20bfff5dc1e8b02ada89e443ecf538a77ff5
Richard Lin <rclin_cal@yahoo.com>
2013-04-05 10:18:00 -0700

    Added comments and renamed title to Fibonacci

--- the-java-programming-language/a-quick-tour/Fibonacci.java
+++ the-java-programming-language/a-quick-tour/Fibonacci.java
@@ -1,10 +1,10 @@
 public class Fibonacci {
-	
+	/** Print out the Fibonacci sequence for values < 50 */	
 	public static void main(String[] args) {
 		int lo = 1;
 		int hi = 1;
 
-		System.out.println("Fibonacci Numbers:");
+		System.out.println("Fibonacci sequence for values < 50:");
 		System.out.println(lo);
 		while (hi < 50) {
 			System.out.println(hi);
