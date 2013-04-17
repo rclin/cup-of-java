77d17c192a83e738bec50146edccfdc4a5002b00
Richard Lin <rclin_cal@yahoo.com>
2013-04-16 18:36:17 -0700

    Added vehicles to linkedlist

--- the-java-programming-language/classes_and_objects/LinkedList.java
+++ the-java-programming-language/classes_and_objects/LinkedList.java
@@ -3,4 +3,25 @@ public class LinkedList {
 	public Object element;
 	public LinkedList next;
 
+	public static void main(String[] args) {
+
+		LinkedList node1 = new LinkedList();
+		LinkedList node2 = new LinkedList();
+
+		Vehicle v1 = new Vehicle();
+		v1.idNum = Vehicle.nextID++;
+		v1.ownerName = "John";
+
+		Vehicle v2 = new Vehicle();
+		v2.idNum = Vehicle.nextID++;
+		v2.ownerName = "Richard";
+		
+		node1.element = v1;
+		node2.element = v2;
+		node1.next = node2;
+
+		System.out.println("node2 vehicle id: " + ((Vehicle)node1.next.element).idNum);
+
+	}
+
 }
