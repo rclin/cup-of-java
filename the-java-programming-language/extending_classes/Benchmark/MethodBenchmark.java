611fe55752e81d2285c1ce769fdc3bbfe3e0fed5
Richard Lin <rclin_cal@yahoo.com>
2013-05-16 11:07:41 -0700

    Added Benchmark (abstract class)

---
+++ the-java-programming-language/extending_classes/Benchmark/MethodBenchmark.java
@@ -0,0 +1,15 @@
+class MethodBenchmark extends Benchmark {
+
+	/** Do nothing, just return **/
+	void benchmark() {
+
+	}
+
+	public static void main(String[] args) {
+
+		int count = Integer.parseInt(args[0]);
+		long time = new MethodBenchmark().repeat(count);
+		System.out.println(count + " methods in " + time + " nanoseconds");
+	}
+
+}
