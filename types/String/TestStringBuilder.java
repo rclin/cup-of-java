30754e2c223cf529f404874a838e04fb92cded03
Richard Lin <rclin_cal@yahoo.com>
2013-06-22 21:04:42 -0700

    Added  String Builder example

---
+++ types/String/TestStringBuilder.java
@@ -0,0 +1,12 @@
+class TestStringBuilder {
+
+	public static void main(String[] args) {
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("Greetings");
+
+		System.out.println(sb.toString());
+	}
+
+}
